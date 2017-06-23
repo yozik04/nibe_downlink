b8ada0012ff856804bc18ebbcc50b01534dbced7
jevgenik <jevgenik@microsoft.com>
2017-06-23 23:47:33 +0300

    Wrapping all in try catch. Otherwise it dies when authentication fails.

--- examples/mqtt.py
+++ examples/mqtt.py
@@ -1,9 +1,12 @@
+import logging
 import time
 
 from paho.mqtt.client import Client as MQTTClient
 
 from config import NIBE_UPLINK_CONF, MQTT_CONF
 from nibe_downlink import NibeDownlink
+
+logger = logging.getLogger()
 
 nd = NibeDownlink(**NIBE_UPLINK_CONF)
 mqtt_client = MQTTClient()
@@ -13,10 +16,13 @@ mqtt_client.connect(MQTT_CONF['hostname'])
 mqtt_client.loop_start()
 
 while True:
-  online, values = nd.getValues()
-  # print values
-  mqtt_client.publish(MQTT_CONF['prefix'] + '/online', 1 if online else 0, retain=True)
-  if values:
-    for key, value in values.iteritems():
-      mqtt_client.publish(MQTT_CONF['prefix'] + '/variables/' + str(key), value, retain=True)
+  try:
+    online, values = nd.getValues()
+    # print values
+    mqtt_client.publish(MQTT_CONF['prefix'] + '/online', 1 if online else 0, retain=True)
+    if values:
+      for key, value in values.iteritems():
+        mqtt_client.publish(MQTT_CONF['prefix'] + '/variables/' + str(key), value, retain=True)
+  except Exception as e:
+    logger.exception("Failed to get Nibe uplink values")
   time.sleep(60)
